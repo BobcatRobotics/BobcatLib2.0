04be93a02c2a070323bf8dc2f8850c0440dfaa5a
GarrenCox <156379648+jha291@users.noreply.github.com>
2024-11-21 20:47:26 -0600

    finally fixed the issue from tn, crash out over.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "BobcatLib2.0";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 377;
-  public static final String GIT_SHA = "f45e682f790227f8ba71d947a8b6208a3edfc29b";
-  public static final String GIT_DATE = "2024-11-21 18:51:24 EST";
+  public static final int GIT_REVISION = 378;
+  public static final String GIT_SHA = "6a4c78158b300aef119beae3b37477b5ce097496";
+  public static final String GIT_DATE = "2024-11-21 19:07:02 EST";
   public static final String GIT_BRANCH = "On-Robot-Development";
-  public static final String BUILD_DATE = "2024-11-21 19:02:42 EST";
-  public static final long BUILD_UNIX_TIME = 1732233762714L;
+  public static final String BUILD_DATE = "2024-11-21 21:46:04 EST";
+  public static final long BUILD_UNIX_TIME = 1732243564646L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
