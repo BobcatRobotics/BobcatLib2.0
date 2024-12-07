0271dee9b46a3ee26a12f80b75bba6801d643070
Arnav Thakrar <arnav.thakrar@gmail.com>
2024-12-07 16:19:31 -0500

    lmao

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "BobcatLib2.0";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 380;
-  public static final String GIT_SHA = "b19d76682592e06f9fb47f95b3dc113eaeb8bf36";
-  public static final String GIT_DATE = "2024-11-22 11:47:26 EST";
+  public static final int GIT_REVISION = 381;
+  public static final String GIT_SHA = "fba3f44cf8558e994127288d79e934c6c303847b";
+  public static final String GIT_DATE = "2024-12-07 16:00:18 EST";
   public static final String GIT_BRANCH = "On-Robot-Development";
-  public static final String BUILD_DATE = "2024-12-07 15:56:43 EST";
-  public static final long BUILD_UNIX_TIME = 1733605003885L;
+  public static final String BUILD_DATE = "2024-12-07 16:17:42 EST";
+  public static final long BUILD_UNIX_TIME = 1733606262204L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
